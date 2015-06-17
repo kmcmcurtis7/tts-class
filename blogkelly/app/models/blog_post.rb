6e13dfc854c30591897ec4bc6486cc1e2fc6b4fe
Kelly M Curtis <Kelly@Kellys-MacBook-Pro.local>
2015-06-17 13:23:27 -0400

    Updated the blogkelly app

--- blogkelly/app/models/blog_post.rb
+++ blogkelly/app/models/blog_post.rb
@@ -1,6 +1,6 @@
 class BlogPost < ActiveRecord::Base
 
-	belongs_to :user
-	has_many :comments
+    belongs_to :user
+    has_many :comments
 
 end
--- blogkelly/app/models/user.rb
+++ blogkelly/app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
-	# Include default devise modules. Others available are:
-	# :confirmable, :lockable, :timeoutable and :omniauthable
-	devise :database_authenticatable, :registerable,
-		:recoverable, :rememberable, :trackable, :validatable
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable
 
-	has_many :blog_posts
-	has_many :comments
+    has_many :blog_posts
+    has_many :comments
 
 end
